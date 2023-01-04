fb8fd79c1142634f8ea133ba4bae1378dde072b8
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-01-04 14:39:37 +0600

    add customs hooks useProducts and load the data on ui successfully

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import useProducts from "../../hooks/useProducts";
 
 const Orders = () => {
-    return (
-        <div>
-            <h2>This is order page</h2>
-        </div>
-    );
+  const [products, setProducts] = useProducts();
+  return (
+    <div>
+      <h2>This is order page: {products.length}</h2>
+    </div>
+  );
 };
 
 export default Orders;
